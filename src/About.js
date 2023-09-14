0c977072750737c08274d814d9f3d8895da775df
J-Kiri <joaogufilho@gmail.com>
2023-09-14 19:40:05 -0300

    start the about page

--- src/About.js
+++ src/About.js
@@ -2,6 +2,6 @@ import './About.css'
 
 export default function About() {
     return(
-        <h1> ABOUT ME </h1>
+        <h1 className="title"> ABOUT ME <span className="p">.</span></h1>
     )
 }
