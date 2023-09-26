40c457dcd478ad373bc0658c4ceec8d50d07eb34
J-Kiri <joaogufilho@gmail.com>
2023-09-26 10:34:10 -0300

    Filter menu almost working

--- src/Work.js
+++ src/Work.js
@@ -1,8 +1,58 @@
 import './Work.css'
-import { Project } from './Projects.js';
+
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import PROJECTBikcraft from "./assets/PROJECT_Bikcraft.png"
+import PROJECTNami from "./assets/PROJECT_Nami.png"
+
+function FilterMenu({categories, selectedCategories, onCategoryClick}) {
+    return(
+        <div className="w_background">
+            <ul className="filter_categories">
+                {categories.map((category, index) => (
+                    <li
+                        key={index}
+                        className={`filter ${selectedCategories === category ? 'active' : ''}`}
+                        onclick={() => onCategoryClick(category)}
+                    >
+                        {category}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+function ProjectGallery({projects, selectedCategory}) {
+    const filteredProjects =
+        selectedCategory === 'All' ? projects :
+            projects.filter(project => project.category.includes(selectedCategory));
+
+    return(
+        <div className="project_gallery">
+            {filteredProjects.map((image, index) => (
+                <img key={index} src={image.url} alt={image.alt}/>
+            ))}
+        </div>
+    ); 
+}
+
 
 export default function Work() {
+    const projects = [
+        {url: PROJECTBikcraft, alt: "Bikcraft", categories: ["UX Design", "UI Design", "Web"]},
+        {url: PROJECTNami, alt: "Nami", categories: ["UX Design", "UI Design", "Mobile App"]},
+    ];
+
+    const allCategories = ["All", "UX Design", "UI Design", "Web", "Mobile App"];
+
+    const [selectedCategory, setSelectedCategory] = useState("All");
+
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+    }
+
     return(
         <body id="work">
             <h1 className="about_title">WORK<span className="p">.</span></h1>
@@ -14,16 +64,14 @@ export default function Work() {
             </h3>
 
             <div className="w_background">
-                <ul className="filter_categories">
-                    <li className="filter">All</li>
-                    <li className="filter">UX Design</li>
-                    <li className="filter">UI Design</li>
-                    <li className="filter">Web</li>
-                    <li className="filter">Mobile App</li>
-                </ul>
+                <FilterMenu
+                    categories={allCategories}
+                    selectedCategories={selectedCategory}
+                    onCategoryClick={handleCategoryClick}
+                />
+
+                <ProjectGallery projects={projects} selectedCategory={selectedCategory} />
             </div>
-            
-            <Project />
         </body>
     )
 }
